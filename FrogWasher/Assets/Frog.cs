e507a252e64128d30e55b8bb7de3a7078f3ff383
elliotf35 <76002379+elliotf35@users.noreply.github.com>
2024-04-05 13:41:28 -0500

    frog healthbar

--- FrogWasher/Assets/Frog.cs
+++ FrogWasher/Assets/Frog.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Frog : MonoBehaviour   
 {
 
-    public int health =  300;
+    public float health = 300;
+    private readonly float initHealth = 300;
     private PlayerProjectileRender ppr;
     public GameObject pwt;
     private Vector2 point;
     private bool firing;
+    private LineRenderer lineRenderer;
+    public Vector2 healthBar0;
+    public Vector2 healthBar1;
+
+    private readonly float width = 0.02f;
     // Start is called before the first frame update
     void Start()
     {
         ppr = pwt.GetComponent<PlayerProjectileRender>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2; // Two points: start and end
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+        healthBar0 = new(transform.position.x - .2f, transform.position.y + .2f);
+        healthBar1 = new(healthBar0.x + .4f, healthBar0.y);
+        lineRenderer.SetPosition(0, healthBar0);
+        lineRenderer.SetPosition(1, healthBar1);
     }
 
     // Update is called once per frame
@@ -23,6 +37,12 @@ public class Frog : MonoBehaviour
         firing = ppr.firing;
         if (firing && IsWithin(point)){
             health -= 1;
+            float percent = health / initHealth;
+            float offset = Mathf.Max(0, 0.4f * percent);
+            Debug.Log("Offset" + offset);
+            Debug.Log("Percent" + percent);
+            healthBar1 = new(healthBar0.x + offset, healthBar0.y);
+            lineRenderer.SetPosition(1, healthBar1);
         }
         if (health <= 0) {
             BoxCollider2D bc = gameObject.GetComponent<BoxCollider2D>();
