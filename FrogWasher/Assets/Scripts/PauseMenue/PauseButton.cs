9a0023e68409441ab5dfe44cfb56fea3ce90606d
Nicholas Domingue <112515521+NichoDA@users.noreply.github.com>
2024-04-22 22:20:36 -0500

    pause menu added

---
+++ FrogWasher/Assets/Scripts/PauseMenue/PauseButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home(){
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
