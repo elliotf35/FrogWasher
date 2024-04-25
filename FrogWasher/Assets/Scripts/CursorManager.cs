353c54be46df9c93b9ee01d6d71bd79856d15947
Patrick Hamilton <phamil4@lsu.edu>
2024-04-25 16:26:04 -0500

    More UI Changes

---
+++ FrogWasher/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class CursorManager : MonoBehaviour
+{
+    /*
+    public Texture2D hand;
+    public void OnButtonCursorEnter(){
+        Cursor.SetCursor(hand,Vector2.zero, CursorMode.Auto);
+    }
+    public void OnButtonCursorExit(){
+        Cursor.SetCursor(hand,Vector2.zero, CursorMode.Auto);
+    }
+    */
+}
