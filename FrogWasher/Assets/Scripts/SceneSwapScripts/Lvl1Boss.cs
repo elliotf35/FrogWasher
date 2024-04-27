cc788d4a550f64f597df970e8390027d743aef5c
Nicholas Domingue <112515521+NichoDA@users.noreply.github.com>
2024-04-27 16:46:30 -0500

    scene transitions
    when editing a scene, select the canvas and disable the crossfade component to see the scene. Re-enable it before testing scene for the load transition to play.

--- FrogWasher/Assets/Scripts/SceneSwapScripts/Lvl1Boss.cs
+++ FrogWasher/Assets/Scripts/SceneSwapScripts/Lvl1Boss.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransitionlvl1Boss : MonoBehaviour
 {
+    public Animator transitionAnimator;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level1Boss");
+              StartCoroutine(TransitionToScene(3));
         }
+    }
+
+    IEnumerator TransitionToScene(int sceneIndex)
+    {
+      
+    transitionAnimator.SetTrigger("Start");
+
+    yield return new WaitForSeconds(1f);
+
+    SceneManager.LoadSceneAsync(sceneIndex);
+
     }
 }
--- FrogWasher/Assets/Scripts/SceneSwapScripts/TitleScreen.cs
+++ FrogWasher/Assets/Scripts/SceneSwapScripts/TitleScreen.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuToTutorial : MonoBehaviour
+public class MenuToGame : MonoBehaviour
 {
+    public Animator transitionAnimator;
+
     public void PlayTutorial(){
-        SceneManager.LoadSceneAsync(1);
+        StartCoroutine(TransitionToScene(1));
     }
 
     public void PlayGame(){
-        SceneManager.LoadSceneAsync(2);
+        StartCoroutine(TransitionToScene(2));
+    }
+
+    IEnumerator TransitionToScene(int sceneIndex)
+    {
+      
+    transitionAnimator.SetTrigger("Start");
+
+    yield return new WaitForSeconds(1f);
+
+    SceneManager.LoadSceneAsync(sceneIndex);
+
     }
 
     public void QuitGame(){
