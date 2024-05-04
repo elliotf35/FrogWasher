b6154a4ae6106c4b56c2c4d769cfce2a5c5f90f5
Nicholas Domingue <112515521+NichoDA@users.noreply.github.com>
2024-05-04 01:55:26 -0500

    fixed blurry sprite bug

---
+++ FrogWasher/Assets/Scripts/mouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+
+        transform.SetAsLastSibling();
+    }
+}
